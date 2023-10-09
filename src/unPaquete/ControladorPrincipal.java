f70da48736c0875c0f7a97dc4c2658eb67b93fb6
tomaslcabrera <tomaslcabrera27@gmail.com>
2023-10-09 14:02:15 -0300

    tp12

--- src/unPaquete/ControladorPrincipal.java
+++ src/unPaquete/ControladorPrincipal.java
@@ -2,19 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package practica23.g12;
+package unPaquete;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author tomyl
  */
-public class Practica23G12 {
+public class ControladorPrincipal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+        Empleado e1 = new Empleado();
+        e1.setNombre("pepito");
+        empleados.add(e1);
+        for (Empleado cancion : empleados) {
+        System.out.println(cancion.getNombre());
+        }
     }
     
 }
